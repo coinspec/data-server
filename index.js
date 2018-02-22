a8b36e995bf51143e2fc400752a56cc5811bae8a
Jan Stránský <jan.stransky@arnal.cz>
2018-02-22 08:53:53 +0000

    update

--- index.js
+++ index.js
@@ -3,31 +3,37 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const http = require('http')
 const path = require('path')
+const MongoClient = require('mongodb').MongoClient
 
 const graphqlServerExpress = require('graphql-server-express')
 const graphql = require('graphql')
 
 const Data = require('coinspec-data-tools').Data
 const data = new Data(path.join(process.cwd(), '../data'))
-data.load()
+
+const mongoUrl = 'mongodb://localhost:27017'
 
 const PORT = 3020
 
-//const graphQLschema = require('./schema')
-const graphQLschema = require('./lib/genschema')(data.collections, data)
 
-var app = express()
-app.use(cors())
+MongoClient.connect(mongoUrl, (err, client) => {
+  const db = client.db('coinspec')
+  const graphQLschema = require('./lib/genschema')(data.collections, db)
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+  var app = express()
+  app.use(cors())
 
-app.use('/graphql', graphqlServerExpress.graphqlExpress({ schema: graphQLschema }))
-app.use('/graphiql', graphqlServerExpress.graphiqlExpress({
-  endpointURL: '/graphql'
-}))
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(bodyParser.json())
 
-const server = http.createServer(app)
-server.listen(PORT, () => {
-  console.log('Server started')
+  app.use('/graphql', graphqlServerExpress.graphqlExpress({ schema: graphQLschema }))
+  app.use('/graphiql', graphqlServerExpress.graphiqlExpress({
+    endpointURL: '/graphql'
+  }))
+
+  const server = http.createServer(app)
+  server.listen(PORT, () => {
+    console.log('Server started')
+  })
 })
+
